82965ee1910e0d822520082c312ceebc8cdc56e0
Crystal Wang <cwang4@stuy.edu>
2019-09-15 00:35:05 -0400

    Added request matching

--- webserver.py
+++ webserver.py
@@ -1,11 +1,47 @@
 from flask import Flask
 from flask import request
+from Request import Request
 
 import json
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
+
+pending_requests = []
+
+def fulfill_request(request, other_requests, fulfilled_requests):
+    for req in other_requests:
+        if req.match(request):
+            fulfilled_requests.append((request, req))
+            other_requests.remove(req)
+            return other_requests, fulfilled_requests
+    other_requests.append(request)
+    return other_requests, fulfilled_requests
+
 def index():
     info = json.loads(request.data)
-    return "{a:1}"
+    fulfilled_requests = []
+    timesC = {
+        "Monday": ["18"],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+        "Saturday": [],
+        "Sunday": [],
+    }
+    kerb = info["kerb"]
+    course = info["course"]
+
+    pending_requests.append(Request(kerb, course, 5, timesC))
+    pending_requests, fulfilled_requests = fulfill_request(pending_requests[0], pending_requests[1:], fulfilled_requests)
+
+    req = fulfilled_requests[0][1]
+
+    return_info = {
+        "kerb": req.get_name(),
+        "course": req.get_course(),
+    }
+
+    return json.dumps(return_info)
